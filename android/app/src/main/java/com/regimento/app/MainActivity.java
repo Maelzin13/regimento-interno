c210fd09e35273577728dfd043eb0c6f957768e3
Ismael dos Santos Dias <mael10774@gmail.com>
2025-04-19 20:06:39 -0300

    Remove capacitor-google-auth dependency from package.json

--- android/app/src/main/java/com/regimento/app/MainActivity.java
+++ android/app/src/main/java/com/regimento/app/MainActivity.java
@@ -1,18 +1,6 @@
 package com.regimento.app;
 
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-
-import java.util.ArrayList;
-
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
 public class MainActivity extends BridgeActivity {
-
-    @Override
-    public void onCreate(android.os.Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        this.registerPlugin(GoogleAuth.class);
-    }
 }
