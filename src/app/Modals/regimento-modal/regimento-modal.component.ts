3fa149bfae99565863bee0f5dc8d922528798fd0
Ismael Dias <mael10774@gmail.com>
2025-06-13 00:34:13 -0300

    feat: adicionar novos índices temáticos e esquemáticos no modal de regimento, além de melhorias no layout da página de autor para melhor apresentação

--- src/app/Modals/regimento-modal/regimento-modal.component.ts
+++ src/app/Modals/regimento-modal/regimento-modal.component.ts
@@ -10,45 +10,6 @@ export class RegimentoModalComponent {
   @Input() type: string = '';
   
   content: any = {
-    autor: {
-      title: 'O Autor',
-      content: `
-        <h3>Sobre o Autor</h3>
-        <p>Secretário Executivo de Comissão na Câmara dos Deputados. Advogado, graduado em Filosofia, pós-graduado em Direito Público e Direito Constitucional. Autor do Regimento Interno Facilitado da Câmara dos Deputados.</p>
-        <br>
-        <h3>Regimento Interno Comentado</h3>
-        <p>Uma abordagem simplificada e interativa do RegimentoInterno da Câmara dos Deputados.</p>
-      `
-    },
-    apresentacao: {
-      title: 'Apresentação',
-      content: `
-       <p class="text-lg text-black">Esta obra é resultado da experiência do Autor do Livro Regimento Interno
-          Facilitado que ao longo dos trabalhos e dos anos de atuação no Plenário e, por último, nas
-          comissões, vinha sentido falta de um suporte teórico que abordasse com um pouco mais de liberdade
-          alguns temas do processo legislativo. A necessidade de assentar alguns posicionamentos pessoais,
-          algumas experiências, algumas críticas e também tecer algumas considerações elogiosas fez com que os
-          escritos particulares do Autor fossem ficando denso de conteúdos, argumentos e teses. O espírito
-          acadêmico impeliu então o Autor a socializar as informações e experiências adquiridas para que os
-          demais operadores do processo legislativo também tivessem conhecimento desse acervo.</p>
-        <p class="text-lg text-black">De fato, o livro Regimento Interno Facilitado, publicado inicialmente
-            pelo Autor em 2011 e nas edições anteriores com os autores Nilvia Caldeira, Cristiano Feu e Ruthier
-            Silva, serviu de base para as importantes melhorias na abordagem e no acréscimo de informações que,
-            mesmo sendo de domínio público, necessitavam ser abordadas de maneira mais direta, mais simples e
-            mais didática, desvencilhando um pouco das aspas e literalidades dos textos normativos, por uma
-            apresentação mais didática.</p>
-        <p class="text-lg text-black">Ao mesmo tempo, a necessidade de ajustar as ferramentas ao momento
-            tecnológico que a humanidade vive passou a exigir que o trabalho fosse apresentado em formato
-            digital, o que está sendo feito nesta obra com a publicação do livro físico e também em aplicativo
-            para android, ios e para acesso na web. Desse modo, esta obra passa a ser o primeiro Regimento
-            Interno, nesse formato, a ser publicada digitalmente por meio de aplicativos.</p>
-        <p class="text-lg text-black">A obra, além de dar uma formatação totalmente diferente na apresentação
-            dos conteúdos, traz comentários e informações que certamente vão auxiliar na compreensão crítica dos
-            temas, podendo inclusive servir de argumento para aperfeiçoamento nas regras do processo legislativo
-            e para defesa de teses em questões de ordem e outras atuações parlamentares no exercício do mandato.
-        </p>
-      `
-    },
     abreviaturas: {
       title: 'Abreviaturas',
       content: `
@@ -123,6 +84,22 @@ export class RegimentoModalComponent {
                   </ul>
               </div>
           </div>
+      `
+    },
+    resumo: {
+      title: 'Índice dos Resumos temáticos',
+      content: `
+        <div>
+          <h3 class="text-xl font-bold text-[#1E999A] mb-4">Índice dos Resumos temáticos</h3>
+        </div>
+      `
+    },
+    esquematico: {
+      title: 'Índice dos Quadros esquemáticos',
+      content: `
+        <div>
+          <h3 class="text-xl font-bold text-[#1E999A] mb-4">Índice dos Quadros esquemáticos</h3>
+        </div>
       `
     }
   };
